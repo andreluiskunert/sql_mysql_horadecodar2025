897d2160a7e1221f01da177dc88433b734cc4f5c
andreluiskunert <pesquisador036@gmail.com>
2025-05-01 15:01:27 -0300

    ArquivosSQL1

---
+++ exercicios/comandospeloworkbench.sql
@@ -0,0 +1,16 @@
+-- CREATE DATABASE sql_mysql_horadecodar;
+-- SHOW databases;
+/* INSERT INTO cadastro('nome', 'email', 'profissao', 'idade', 'descricao', 'endereco' )  
+VALUES(
+   "Matheus",
+   "mat036@gmail.com",
+   "Programador",
+   42,
+   "trabalhar para a Microsoft",
+   "morar no Paraguay"
+); */
+ --  CREATE TABLE pessoas(nome VARCHAR(100), email VARCHAR(150), cpf CHAR(12), profissao VARCHAR(250), descricao MEDIUMTEXT, idade INT(2), endereco VARCHAR(100));
+
+--  SELECT * FROM pessoas;
+ -- CREATE TABLE testetabela(testando varchar(100));
+  -- DROP TABLE testetabela; obs.: sempre fazer backup ...alter
---
+++ exercicios/curso_sql_myql_workbench_hora_de_codar.sql
@@ -0,0 +1,25 @@
+ -- CREATE DATABASE sql_e_mysql_workbench_hora_de_cordar;
+ /*
+  Obs.: explica sobre assuntos
+ */
+ -- SHOW databases; exbir todos os bancos que existem...
+ --  DROP DATABASE cursoDesqlDaHoradeCodar; remover o banco de dados;
+-- CREATE DATABASE meuComputador;
+-- SHOW DATABASES;
+-- DROP DATABASE meuComputador;
+-- SHOW DATABASES;
+ /* USE sql_e_mysql_workbench_hora_de_codar; 
+ =============
+ § Explica e a faça a introdução ao modulo§ */
+ CREATE TABLE cadastro(
+ nome  VARCHAR(100),
+ email  CHAR(150),
+ cpf CHAR(11),
+ profissao VARCHAR(100),
+  idade INT(2),
+  descricao MEDIUMTEXT,
+  endereco VARCHAR(255)
+ );
+ 
+ 
+ 
