8f6ae7be66c7684823e073c21cd2f81d1e97c7ed
andreluiskunert <pesquisador036@gmail.com>
2025-05-02 14:48:06 -0300

    salvandoNovosComandos

---
+++ exercicios/GerenciandoBancosDadosTabela/inserindoEcriandoTabela.sql
@@ -0,0 +1,17 @@
+/*   Sexta-feira 02/05/2025 */
+-- CREATE DATABASE teste_dba;
+ -- SHOW databases;
+ -- USE teste_dba;
+ -- CREATE TABLE produtos(nome VARCHAR(100),sku CHAR(5), informacoes MEDIUMTEXT,preco VARCHAR(100) );
+--  CREATE TABLE cliente(nome VARCHAR(100) , sobrenome VARCHAR(100), email VARCHAR(150), idade INT(2), profissao VARCHAR(150), endereco MEDIUMTEXT);
+/*INSERT INTO cliente(nome , sobrenome , email, idade , profissao , endereco )
+ -- VALUES ("Jean", "Cardoso", "jeancardoso02@gmail.com", 25, "Arquiteto", "Rua do ArePorto Pato branco" );
+ -- VALUES ("Lucas", "Camargo", "lucacarmago01@gmail.com", 20, "Eletrecista", "Rua do Planalto nª02 Pato branco" );
+ -- VALUES ("Luis Carlos", "Ramos", "luiscarlos03@gmail.com", 42, "Arquiteto", "Rua do Xingu Pato branco" );
+  VALUES ("André Luis ", "Kunert", "alktecno@gmail.com", 42, "Desenvolvedor Full Stack Java e PHP", "Rua do D. 1º Pedro  Pato branco" );
+  SELECT * FROM cliente;*/
+/*  INSERT INTO produtos(nome, sku, informacoes) VALUES ("sapatos","SP01", "Serve para proteger os pés");
+    INSERT INTO produtos(nome, sku, informacoes) VALUES ("Terno","TR01", "Serve para ir a lugar com estilos ");
+      INSERT INTO produtos(nome, sku, informacoes) VALUES ("Gravata","GT01", "Serve  para usar com o terno e o sapato");
+      */
+      SELECT * FROM produtos;
